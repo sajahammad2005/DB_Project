eedc09783b27bb9be5d92ada1e7a72ec84346dad
randn <randn@Rand>
2026-01-15 16:44:50 +0200

    qw

--- ProjectDatabase/src/application/Main.java
+++ ProjectDatabase/src/application/Main.java
@@ -1,6 +1,5 @@
 package application;
 	
-<<<<<<< HEAD
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -15,28 +14,6 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-=======
-import java.sql.Connection;
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-
-public class Main extends Application {
-	static Connection conn = DBConnect.getConnection();
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			 if (conn != null) {
-		            System.out.println("Connected!");
-		        } else {
-		            System.out.println("X");
-		        }
-			primaryStage.setScene(new CustomerScene(primaryStage).createScene());
-			primaryStage.setTitle("Customer Stage");
->>>>>>> branch 'master' of https://github.com/sajahammad2005/DB_Project
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
